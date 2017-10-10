489ac00cb7f6e798c332e01c0fdcdb47d7a552ba
Suyi Thonatos.Yang <thonatos.yang@gmail.com>
2017-10-10 16:20:18 +0800

    test: add tests

--- __tests__/app.js
+++ __tests__/app.js
@@ -3,15 +3,63 @@ var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
-describe('generator-eggjs:app', () => {
+describe('generator-eggjs:simple', () => {
   beforeAll(() => {
-    return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true});
+    return helpers.run(path.join(__dirname, '../generators/app')).withPrompts({
+      type: 'simple',
+      name: 'simple',
+      description: 'simple project'
+    });
   });
 
   it('creates files', () => {
     assert.file([
-      'dummyfile.txt'
+      'app/router.js',
+      'app/controller/home.js',
+      'config/plugin.js',
+      '.gitignore'
     ]);
+  });
+});
+
+describe('generator-eggjs:empty', () => {
+  beforeAll(() => {
+    return helpers.run(path.join(__dirname, '../generators/app')).withPrompts({
+      type: 'empty',
+      name: 'empty',
+      description: 'empty project'
+    });
+  });
+
+  it('creates files', () => {
+    assert.file(['README.md', '.gitignore']);
+  });
+});
+
+describe('generator-eggjs:plugin', () => {
+  beforeAll(() => {
+    return helpers.run(path.join(__dirname, '../generators/app')).withPrompts({
+      type: 'plugin',
+      name: 'plugin',
+      description: 'egg plugin'
+    });
+  });
+
+  it('creates files', () => {
+    assert.file(['README.md', '.gitignore']);
+  });
+});
+
+describe('generator-eggjs:framework', () => {
+  beforeAll(() => {
+    return helpers.run(path.join(__dirname, '../generators/app')).withPrompts({
+      type: 'framework',
+      name: 'framework',
+      description: 'egg framework'
+    });
+  });
+
+  it('creates files', () => {
+    assert.file(['README.md', 'app/extend/context.js', '.gitignore']);
   });
 });
