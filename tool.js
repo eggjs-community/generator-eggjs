41b6c03407f287afaa627374f07e68b17b447da4
duncup <dlmsoft.wolf@gmail.com>
2018-04-17 15:04:14 +0800

    refactor: use fs-plus package to simplify code.

--- tool.js
+++ tool.js
@@ -1,45 +1,19 @@
 'use strict';
 const fs = require('fs');
+const fsPlus = require('fs-plus');
 const Path = require('path');
 
-// eslint-disable-next-line no-extend-native
-Array.prototype.remove = function(val) {
-  var index = this.indexOf(val);
-  if (index > -1) {
-    this.splice(index, 1);
-  }
-  return this;
-};
-
+const list = fsPlus.listTreeSync(Path.join(__dirname, 'generators/app/templates'));
 // eslint-disable-next-line no-unused-vars
 function generatorFile(fileName) {
-  const file = fs.readFileSync(fileName);
-  let over = file.toString().replace(/{{/g, '<%- ');
-  over = over.replace(/}}/g, ' %>');
-  return fs.writeFileSync(fileName, over);
+  try {
+    const file = fs.readFileSync(fileName);
+    let over = file.toString().replace(/{{/g, '<%- ');
+    over = over.replace(/}}/g, ' %>');
+    return fs.writeFileSync(fileName, over);
+  } catch (err) {}
 }
-
-function getNameWithPath(path) {
-  const files = fs.readdirSync(path).remove('.DS_Store');
-  return files.map(function(item) {
-    try {
-      const subFiles = getNameWithPath(Path.join(path, item));
-      return subFiles;
-    } catch (err) {
-      return Path.join(path, item);
-    }
-  });
-}
-
-function forEachTemplate() {
-  const templatePath = Path.join(__dirname, 'generators/app/templates');
-  const files = getNameWithPath(templatePath);
-  return files;
-}
-forEachTemplate()
-  .join(',')
-  .split(',')
-  .forEach(function(item) {
-    generatorFile(item);
-  });
+list.forEach(function(item) {
+  generatorFile(item);
+});
 console.log('tool run over.');
